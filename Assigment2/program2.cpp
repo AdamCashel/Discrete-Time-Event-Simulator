e776b8bbe99d7455ed32650ae36f9656275f16d6
Adam Cashel <67659373+AdamCashel@users.noreply.github.com>
2021-11-09 00:14:56 -0600

    bug fix

--- Assigment2/program2.cpp
+++ Assigment2/program2.cpp
@@ -3,7 +3,6 @@
 #include <math.h>
 #include <time.h>   
 #include <iostream>
-#include <fstream>
 
 ////////////////////////////////////////////////////////////////
 // sample events
@@ -58,7 +57,6 @@ float process_lamda;
 float average_arrival;
 float quantum_number;
 float Total_turnaround = 0;
-float total_throughput = 0;
 int algorithm_type;
 bool server_idle; //replace with using process* server pointing to the process being serviced, NULL indicates idle
 int readyque_count = 0; //Replace with FIFO queue (or more general, a priority queue) of processes (pointers to processes)
@@ -123,13 +121,7 @@ void process_event2(struct event* eve)
 {
 	//collect data
 	Total_turnaround += (clock1 - eve->p->arrivalTime);
-	//std::cout << "Total Check: " << clock1 << " " << eve->enter_time << std::endl;
-
-	if(head->next == NULL)
-	{
-		float total_time = eve->enter_time;
-		total_throughput = 10000/total_time;
-	}
+	std::cout << "Total Check: " << clock1 << " " << eve->enter_time << std::endl;
 }
 
 ////////////////////////////////////////////////////////////////
@@ -189,7 +181,7 @@ void init()
 		process* process_temp = new process;
 		//process_temp->arrivalTime =
 		//std::cout << "start of init222" << std::endl;
-		process_temp->id = i + 1;
+		process_temp->id = rand() % 900000 + 1;
 		//std::cout << "start of init2333" << std::endl;
 		process_temp->remainingServiceTime = process_temp->arrivalTime;
 		process_temp->serviceTime = 0;
@@ -211,7 +203,7 @@ void init()
 	//Print linked list to check if in order of time
 	/*struct event* temp4 = new event;
 	temp4 = head;
-	
+
 	std::cout << "Before Check" << std::endl;
 	for (int i = 0; i < 5; i++)
 	{
@@ -228,12 +220,6 @@ void generate_report()
 	// output statistics
 	float average_turnaround = 10000 / Total_turnaround;
 	std::cout << "Average Turnaround: " << average_turnaround << std::endl;
-	std::cout << "Total Throughput: " << total_throughput << std::endl;
-
-	std::ofstream report_file;
-	report_file.open("report.csv", std::ofstream::app);
-	report_file << process_lamda << ", " << average_turnaround << ", " << total_throughput << std::endl;
-	report_file.close();
 }
 //////////////////////////////////////////////////////////////// 
 //schedules an event in the future
@@ -269,14 +255,14 @@ void schedule_event(struct event* new1)
 			previous_node->next = new1;
 			new1->next = current_node;
 		}
-		
+
 	}
 }
 
 ////////////////////////////////////////////////////////////
 int run_sim()
 {
-	
+
 	struct event* eve;
 	while (head)
 	{
@@ -314,7 +300,7 @@ int main(int argc, char* argv[])
 	//Fourth Argument: quantum value (used for RR only)
 	//parse arguments
 	//If algorithm_type == 1 run First Come First Serve , If algorithm_type == 2 run Shortest Remaining Time First, If algorithm_type == 3 run Round Robin with quantum value
-	
+
 	std::cout << "start" << std::endl;
 	algorithm_type = atoi(argv[1]);
 	process_lamda = atoi(argv[2]);
